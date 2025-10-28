cda87ff3af69e57b566183ac1c000ed173ea38bd
ptetting <ptetting@uwm.edu>
2025-10-28 15:32:40 -0500

    something wrong

--- Project_RoomBookingSystem/urls.py
+++ Project_RoomBookingSystem/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from booking_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,9 +1,18 @@
+from django.views import View
 from django.shortcuts import render
 from .models import Room, Booking
 from datetime import date
 
-def home(request):
-    today = date.today()
-    bookings = Booking.objects.filter(start_time__date=today)
-    rooms = Room.objects.all()
-    return render(request, 'booking_app/home.html', {'rooms': rooms, 'bookings': bookings})
+class HomeView(View):
+    template_name = 'booking_app/home.html'
+
+    def get(self, request):
+        today = date.today()
+        bookings = Booking.objects.filter(start_time__date=today)
+        rooms = Room.objects.all()
+        context = {
+            'today': today,
+            'rooms': rooms,
+            'bookings': bookings,
+        }
+        return render(request, self.template_name, context)
